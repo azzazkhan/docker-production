3d5c1d79f6695349677cb83366acb39f9e5b49bd
Azzaz Khan <hello@azzaz.dev>
2025-08-16 00:30:30 +0500

    wip: Added scripe for clone provisioning server

---
+++ scripts/clone.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+set -eu
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <hostname>"
+    exit 1
+fi
+
+REMOTE_HOST="$1"
+
+scp scripts/provision.sh root@"$REMOTE_HOST":provision.sh
+ssh -t root@"$REMOTE_HOST" "bash provision.sh"
+
+zip -r9 docker.zip scripts volumes .env.example docker-compose*.yml
+scp docker.zip deployer@"$REMOTE_HOST":docker.zip
+
+rm -rf docker.zip
+
+ssh -t deployer@"$REMOTE_HOST" "unzip docker.zip -d docker && cd docker && bash scripts/setup.sh && docker compose pull && docker compose -f docker-compose.http.yml up -d traefik"
